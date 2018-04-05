50a21c31c0f765b8658457c0710ed76a2b2a30df
Sai Kandala <skandala@SemGeeks.local>
2018-04-05 16:39:51 -0400

    Initial commit

---
+++ circle/js.js
@@ -0,0 +1,12 @@
+function draw() {
+  var canvas = document.getElementById("multiplier");
+  var ctx = canvas.getContext("2d");
+
+  var x = 300; // x coordinate
+  var y = 300; // y coordinate
+  var radius = 295; // Arc radius
+  var point_size = 2;
+  var number_of_points = 10;
+  var multiplier = 2;
+  var points = [];
+}
