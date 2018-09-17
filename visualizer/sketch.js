369ba04fd23409c14478ed1bb4e451e689ab839a
saik212 <kandala.sai@gmail.com>
2018-09-17 14:53:20 -0400

    Adds visualizer

--- visualizer/sketch.js
+++ visualizer/sketch.js
@@ -2,11 +2,17 @@ function setup (){
     song = loadSound('../sounds/lair.mp3', loaded);
     amp = new p5.Amplitude();
     createCanvas(640, 480);
+    fft = new p5.FFT();
 }
 
 function draw (){
-    stroke(155);
-    ellipse(100, 100, amp.getLevel() * 250, amp.getLevel() * 250);
+    spectrum = fft.analyze();
+    // stroke(155);
+    // ellipse(100, 100, amp.getLevel() * 250, amp.getLevel() * 250);
+    // rect(100,600,20, -spectrum[0]);
+    for (let i = 0; i <= spectrum.length; i++){
+        rect(10 + i*2, 500, 20, -spectrum[i]);
+    }
 }
 
 function loaded (){
